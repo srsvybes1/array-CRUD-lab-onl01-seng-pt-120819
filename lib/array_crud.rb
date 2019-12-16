4c6ce4f20b00b66b3a0975d10a0aae6a0677a255
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-16 16:06:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,34 @@
 def create_an_empty_array
-  
+  create_an_empty_array = []
 end
 
 def create_an_array
-  
+  create_an_array = ["one", "two", "three", "four"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(create_an_array, five)
+  create_an_array ("five")
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(create_an_array, zero)
+  create_an_array.shift ("zero")
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(create_an_array)
+  create_an_array.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(create_an_array)
+  create_an_array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(create_an_array, element)
+  create_an_array = [2]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(create_an_array)
+  create_an_array [0]
 end
 
-def retrieve_last_element_from_array(array)
-  
-end
+def retrieve_last_element_from_array(create_an_array)
+  create_an_array [4]
